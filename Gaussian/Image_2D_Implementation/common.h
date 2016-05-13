7108dfa62333d0c25372c40de5afb1d1d9979324
sumeet dube <dubesumeet2@gmail.com>
2016-05-13 12:51:32 +0530

    Image2D implementation according to OPenCl 1.2, change clCreateImage function for OpenCL 1.1 EP

---
+++ Gaussian/Image_2D_Implementation/common.h
@@ -0,0 +1,12 @@
+/* including header files that are commonly used */
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstring>
+#include<stdlib.h>
+#include<math.h>
+#include<cstring>
+#include<unistd.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+using namespace std;
